d66a1058703de932e670efb05d4af6cbd8930f10
Hufan <hufany21@gmail.com>
2021-11-06 01:04:02 -0400

    add ip

---
+++ index.js
@@ -0,0 +1,16 @@
+const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
+
+fetchCoordsByIP('90.2474', (error, data) => {
+  console.log(error, data);
+});
